ae9010edeadb97db2f71b9ba6717e7ed592b98c0
MelinaFigueroa <melina.figueroa.89@gmail.com>
2023-12-29 17:57:03 -0300

    se agrega render y conf

--- server.js
+++ server.js
@@ -6,16 +6,11 @@ require('dotenv').config();
 
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
+// Usar la cadena de conexión desde las variables de entorno
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true, 
   useUnifiedTopology: true,
-  // Añade esta opción para proporcionar las credenciales
-  auth: {
-    user: process.env.MONGODB_USER,
-    password: process.env.MONGODB_PASSWORD
-  }
 });
-
 
 // Middleware para analizar solicitudes JSON
 app.use(express.json());
